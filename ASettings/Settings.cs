eeb482c01d2dbac29dbe1927b9255b5f7abe9049
Adrian Sims <adriansims62781@gmail.com>
2020-06-09 10:56:47 +0200

    Updates (including unit tests).

---
+++ ASettings/Settings.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ASettings
+{
+    /// <summary>
+    /// global settings are specified in the global section and apply everywhere
+    /// other sections can be swapped in our out. We have a dictionary for global
+    /// setting and a dictionary for each section. So in effect we need 
+    /// Dictionary<string, Dictionary<string, List<string>>
+    /// </summary>
+    public class Settings
+    {
+        private readonly string _settingsFile;
+        Dictionary<string, Dictionary<string, List<string>>> _settings = new Dictionary<string, Dictionary<string, List<string>>>();
+
+        List<(int linenumber, string line)> _errors = new List<(int linenumber, string line)>();
+        public Settings(string filename)
+        {
+            this._settingsFile = filename;
+
+            var currentSection = "";
+            var linenumber = 0;
+            foreach (var (line, skip, equalsCount) in File.ReadLines(_settingsFile).Select(ProcessLine))
+            {
+                linenumber++;
+                if (skip)
+                {
+                    continue;
+                }
+                if (line.First() == '[')
+                {
+                    if (line.Last() != ']')
+                    {
+                        _errors.Add((linenumber, line));
+                        continue;
+                    }
+                    currentSection = line.Substring(1, line.Length - 2);
+                }
+                else if (equalsCount != 1)
+                {
+                    _errors.Add((linenumber, line));
+                    continue;
+                }
+                else if (string.IsNullOrEmpty(currentSection))
+                {
+                    _errors.Add((linenumber, line));
+                    continue;
+                }
+                else
+                {
+                    AddDictionaryEntry(currentSection, line);
+                }
+            }
+
+        }
+
+        public string[] Sections => _settings.Keys.ToArray();
+
+        public (int linenumber, string line)[] Errors => _errors.ToArray();
+
+        public string[] Setting(string section, string name)
+        {
+            if (_settings.TryGetValue(section, out var innerDict))
+            {
+                if (innerDict.TryGetValue(name, out var list))
+                {
+                    return list.ToArray();
+                }
+            }
+            return new string[] { };
+        }
+
+        private void AddDictionaryEntry(string currentSection, string line)
+        {
+            if (!_settings.TryGetValue(currentSection, out var innerDict))
+            {
+                innerDict = new Dictionary<string, List<string>>();
+                _settings.Add(currentSection, innerDict);
+            }
+
+            var segments = line.Split('=').Select(x => x.Trim()).ToArray();
+            if (!innerDict.TryGetValue(segments[0], out var list))
+            {
+                list = new List<string>();
+                innerDict.Add(segments[0], list);
+            }
+            list.Add(segments[1]);
+        }
+
+        private (string line, bool skip, int equalsCount) ProcessLine(string line)
+        {
+            var equalCount = 0;
+            if (string.IsNullOrEmpty(line))
+            {
+                return (line, true, 0);
+            }
+
+            var trimmed = line.Trim();
+            if (trimmed.StartsWith("#"))
+            {
+                return (trimmed, true, 0);
+            }
+
+            var quote = 0;
+            int count = 0;
+            foreach (var c in trimmed)
+            {
+                if (quote == 0)
+                {
+                    if (c == '#')
+                    {
+                        break;
+                    }
+                    else if (c == '\'')
+                    {
+                        quote = 1;
+                    }
+                    else if (c == '"')
+                    {
+                        quote = 2;
+                    }
+                    else if (c == '=')
+                    {
+                        equalCount++;
+                    }
+                }
+                else if (quote == 1)
+                {
+                    if (c == '\'')
+                    {
+                        quote = 0;
+                    }
+                }
+                else if (quote == 2)
+                {
+                    if (c == '"')
+                    {
+                        quote = 0;
+                    }
+                }
+
+                count++;
+            }
+
+            return (trimmed.Substring(0, count), false, equalCount);
+        }
+    }
+}
---
+++ ASettingsTests/SettingsTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASettings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using FluentAssertions;
+
+namespace ASettings.Tests
+{
+    [TestClass()]
+    public class SettingsTests
+    {
+        [TestMethod()]
+        public void SettingsTest()
+        {
+            var errorLine = "john = fred = 1";
+            var contents = new List<string>
+            {
+                "[alpha]",
+                "fred=jim",
+                "fred=harry",
+
+                " [beta]",
+                "harry = sally",
+                "# testing",
+                "mary = we#ndy",
+                "mary = 'we#ndy'",
+                errorLine
+            };
+
+            var filename = Path.GetTempFileName();
+            File.WriteAllLines(filename, contents);
+            var s = new Settings(filename);
+            var sections = s.Sections;
+            sections.Should().BeEquivalentTo("alpha", "beta");
+
+            var s1 = s.Setting("beta", "mary");
+            s1.Should().HaveCount(2).And.BeEquivalentTo("we", "'we#ndy'");
+            var s2 = s.Setting("beta", "joseph");
+            s2.Should().HaveCount(0);
+            var s3 = s.Setting("Raymond", "Smith");
+            s3.Should().HaveCount(0);
+            s.Errors.Should().HaveCount(1);
+
+            var errorLineNumber = contents.IndexOf(errorLine);
+            s.Errors[0].linenumber.Should().Equals(errorLineNumber);
+            s.Errors[0].line.Should().Equals(errorLine);
+
+        }
+
+        [TestMethod()]
+        public void SettingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
