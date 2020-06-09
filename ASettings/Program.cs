8d919c8b10ac4ced1eca67543df7db83dd7218ca
Adrian Sims <adriansims62781@gmail.com>
2020-06-09 11:01:20 +0200

    Removed unnecessary usings.

--- ASettings/Program.cs
+++ ASettings/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using System.Threading.Tasks;
 
 namespace ASettings
 {
